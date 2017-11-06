e0122bd38e84c9b4c0b813e8f014ac68deaebf98
luferrari <lucialuferrari@gmail.com>
2017-11-06 14:52:33 +0100

    final

--- sketch.js
+++ sketch.js
@@ -1,45 +1,55 @@
-var music, clouds, ground, frequency, amplitude;
+var music, pie, sky, frequency, amplitude;
 
 function preload() {
-  clouds = loadImage('./assets/clouds.png');
-  ground = loadImage('./assets/ground.png');
+  pie = loadImage('./assets/pie.png')
+  sky = loadImage('./assets/sky.png');
   music = loadSound('./assets/pie in the sky.mp3');
   // Music by HookSounds
   // see music-readme.txt for more info
 }
 
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   frameRate(45);
+  angleMode(DEGREES);
   noFill();
   noStroke();
 
-  music.play(.75);
-  music.rate(1.08);
+  // music.play([startTime],[rate],[amp],[loopStart],[duration]);
+  music.loop(0.7, 1.16, 1, 0, 152);
   frequency = new p5.FFT();
   frequency.setInput(music);
   amplitude = new p5.Amplitude();
   amplitude.setInput(music);
 }
 
+
+
 function draw() {
+  var co = 3;
+  var si = 6;
+  var cosine = si * width / (si * 10) * cos(frameCount % 360 * co - 90);
+  var sine = co * width / (co * 30) * sin(frameCount % 360 * si - 22.5);
+  var l = sqrt(width * height);
+
   background(28, 40, 38, 223);
-  for (var s = 0; s < width; s += height * 0.666) {
-    image(ground, s, height * 0.666, height * 0.666, height * 0.333);
-  }
+  // sky
   ampGen();
-  for (var g = 0; g < width; g += height * 0.666) {
-    image(clouds, g, 0, height * 0.666, height * 0.666);
+  for (var s = -width / 2; s < width; s += height * 0.666) {
+    image(sky, s, 0, height * 0.666, height * 0.666);
   }
   freqGen();
 
-  var l = 0;
-  if (!music.isPlaying()) {
-    music.loop(1);
-    l++;
-    console.log(l);
-  }
+  // pie
+  image(pie,
+    cosine + width / 2 - width / 12,
+    sine + height / 6 - height / 18,
+    0.2 * sqrt(width * height), 0.2 * sqrt(width * height));
 }
+
+
 
 function ampGen() {
   var vol = amplitude.getLevel();
@@ -54,20 +64,22 @@ function ampGen() {
   ellipse(width * 0.9, width * 0.1, amp * a * 0.25);
 }
 
+
+
 function freqGen() {
   var spectrum = frequency.analyze();
-  var step = 1.5;
+  var step = 1.25;
 
   noFill();
   push();
   scale(width / 1020, 1);
 
   for (f = 0; f < height * 0.75; f += step) {
-    strokeWeight(step * 1.75);
-    stroke(lerpColor(color(214, 69, 80, 191), color(234, 158, 141, 191), f / 350));
+    strokeWeight(step * 1.25);
+    stroke(lerpColor(color(214, 69, 80, 223), color(234, 158, 141, 223), f / 350));
 
     push();
-    translate(0, f * 1.25);
+    translate(0, f * 1);
     beginShape();
     for (v = 0; v < spectrum.length; v += 20) {
       vertex(v, map(spectrum[v], 0, 255, height * 0.666, height * 0.25));
@@ -78,6 +90,8 @@ function freqGen() {
   }
   pop();
 }
+
+
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
